7bcd890aebb8abbc51e6900c5b7278970b103aa5
PatrickCraveiro <patrick.craveiro@medgrupo.com.br>
2021-05-28 01:31:29 -0300

    TCC backend

---
+++ server.js
@@ -0,0 +1,20 @@
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+require("./app/routes/customer.routes.js")(app);
+
+// set port, listen for requests
+app.listen(3050, () => {
+  console.log("Server is running on port 3050.");
+});
