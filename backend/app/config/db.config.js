338ea3810a9962991254c63ba5bd4c32bc7b1c67
PatrickCraveiro <patrick.craveiro@medgrupo.com.br>
2021-05-30 00:08:12 -0300

    test API WEB

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
+    HOST: "mysqlserver.cd4xosi0frgh.sa-east-1.rds.amazonaws.com",
+    USER: "admin",
     PASSWORD: "tickmateus123",
     DB: "tcc"
 };
