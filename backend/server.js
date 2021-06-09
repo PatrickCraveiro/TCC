021e4455e0d9332b959b4dc62260c3978e7b1b84
PatrickCraveiro <patrick.craveiro@medgrupo.com.br>
2021-06-09 06:44:06 -0300

    Ajustes finais

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./app/routes/clinica.routes.js")(app);
 require("./app/routes/login.routes.js")(app);
 require("./app/routes/consulta.routes.js")(app);
-require("./app/routes/funcionario.routes.js")(app);
+// require("./app/routes/funcionario.routes.js")(app);
 
 // set port, listen for requests
 app.listen(3050, () => {
