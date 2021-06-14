0a07dbbb2782cc0cfa98a9647b62b9323fdd5496
PatrickCraveiro <patrick.craveiro@medgrupo.com.br>
2021-06-14 02:00:28 -0300

    Ajustes finais

---
+++ frontend/dev/www/js/main.vue
@@ -0,0 +1,22 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    abaAtiva: "relatorios",
+    curso: '',
+    statusOV: '',
+    ano: '',
+    appsDireito: '',
+    observacoes: '',
+    consultaRealizada: ''
+  },
+  methods: {
+    toogleActive: function () {
+      this.relatoriosActive = !this.relatoriosActive;
+    },
+  },
+  created: function () {
+    // `this` aponta para a instância
+    console.log('a é: ' + this.message)
+  }
+});
